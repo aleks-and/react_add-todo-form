9e1b77c57e3274be74f2eed4f3fffb9b9eb57b10
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-17 23:18:11 +0300

    Updates

---
+++ precache-manifest.ba7bfc029aca89912c92bdb2850046f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579c5f8ecdccace989dcc1d6d8ab7551",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "6888b2e3acab31b40f7e",
+    "url": "/react_add-todo-form/static/css/main.4274f883.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_add-todo-form/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "6888b2e3acab31b40f7e",
+    "url": "/react_add-todo-form/static/js/main.08fa165f.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
